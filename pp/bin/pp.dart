55a9efc26ffb26f2aa548f9a7e90e3580222926c
aritz <aritz_egiguren@hotmail.com>
2020-09-24 10:00:02 +0200

    Example dart project

---
+++ pp/bin/pp.dart
@@ -0,0 +1,28 @@
+void main(List<String> arguments) {
+// Dado un número N, mayor que la suma de los números de una lista L1. 
+// Calcula una nueva Lista L2 cuya suma sea igual a N, y cada valor en L2 sea proporcional al número en L1. (P.D: Es más fácil recursivamente)
+// Ej: Dado N =12, y L1=[1,2,3], la lista a calcular L2 será [2,4,6]
+
+// Extra1:  lo mismo pero N representa un área h=alto x w=ancho. L2 sería una lista de áreas. 
+// Extra2: Y si el área tuviese coordenadas x,y. L2 sería una lista de áreas y coordenadas .  
+
+
+
+
+int CalcularSegundaLista(){
+  return(2);
+}
+
+num n 12;
+17
+const listaDeNumeros = [0, 2, 4];
+6
+const listaDeNumeros = [0, 2, 4];
+17
+
+
+const listaDeNumeros = [0, 4, 8];
+
+print(CalcularSegundaLista());
+
+}
