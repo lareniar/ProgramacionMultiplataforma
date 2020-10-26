231619b9748c6f364c1dc3d7ce512b1ffc2eb488
aritz <aritz_egiguren@hotmail.com>
2020-10-26 12:58:37 +0100

    update

--- T1/calendar/calendar/lib/src/calendar.dart
+++ T1/calendar/calendar/lib/src/calendar.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+        title: 'Material App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Material App Bar'),
+          ),
+          body: ListView(
+            
             children: <Widget>[
-            for(var i=0; i<3; i++)CuadroWidget(),
-            ]),
-        ),
-      ),
-    );
+              for (var i = 0; i < 10; i++)
+              
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                        color: Colors.green,
+                        child: CuadroWidget())),
+            ],
+          ),
+        ));
   }
-  
 }
 
 class CuadroWidget extends StatelessWidget {
@@ -32,28 +38,23 @@ class CuadroWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 100,
+      width: 20,
+      height: 50,
       decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(10)
-      ),
-      child: Column(
+          color: Colors.amber, borderRadius: BorderRadius.circular(10)),
+      child: ListView(
         children: <Widget>[
-
           SizedBox(
             height: 15,
           ),
-          
           Text('Uno'),
           Text('Dos'),
           Expanded(
-            flex: 1,
-            child: Container(
-              width: 0,
-              height: 0,
-            )
-          ),
+              flex: 1,
+              child: Container(
+                width: 0,
+                height: 0,
+              )),
           Text('Tres'),
           SizedBox(
             height: 15,
